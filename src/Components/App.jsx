21dd5cbbe8458da176da23d29cce011e39477b64
GsSteven <65253485+GsSteven@users.noreply.github.com>
2021-06-28 16:40:05 -0400

    scss boiler and colors chosen

---
+++ src/Components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="appWrapper">
+      <h1 className="color0">Color</h1>
+      <h1 className="color1">Color</h1>
+      <h1 className="color2">Color</h1>
+      <h1 className="color3">Color</h1>
+      <h1 className="color4">Color</h1>
+    </div>
+  );
+}
+
+export default App;
