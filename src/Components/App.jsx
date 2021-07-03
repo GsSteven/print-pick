e83c14511093842c93306126c110cbce3b381789
Steven Gsell <steven_gsell@yahoo.com>
2021-07-02 22:00:14 -0400

    added mobile navbar and animations

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import Search from "./Search";
@@ -10,7 +10,10 @@ import fakeServerObject from "./../fakeServer";
 
 function App() {
   const [printers, setPrinters] = useState([]);
-
+  const navBarRef = useRef();
+  const mobileNav1 = useRef();
+  const mobileNav2 = useRef();
+  const mobileNav3 = useRef();
   useEffect(() => {
     setPrinters(fakeServerObject);
   }, []);
@@ -31,11 +34,46 @@ function App() {
     return routes;
   };
 
+  const expandMobileNav = () => {
+    const navBar = navBarRef.current;
+    const navLine1 = mobileNav1.current;
+    const navLine2 = mobileNav2.current;
+    const navLine3 = mobileNav3.current;
+    if(navBar.style.right !== "0%"){
+      navBar.style.right = "0%";
+      navLine1.style.transform = "translateY(10.5px) rotate(-45deg)";
+      navLine1.style.backgroundColor = "#A51A41";
+      navLine2.style.opacity = "0";
+      navLine3.style.transform = "translateY(-10.5px) rotate(45deg)";
+      navLine3.style.backgroundColor = "#A51A41";
+    }else {
+      navBar.style.right = "-110%";
+      navLine1.style.transform = "";
+      navLine1.style.backgroundColor = "white";
+      navLine2.style.opacity = "1";
+      navLine3.style.transform = "";
+      navLine3.style.backgroundColor = "white";
+    }
+  }
+
   return (
     <div className="appWrapper">
       <div className="navBar">
         <p>Print Pick</p>
-        <ul>
+        {window.innerWidth < 1000 &&
+          <div 
+            className="mobileNav"
+            onClick={expandMobileNav}
+          >
+            <div className="navLine" ref={mobileNav1}/>
+            <div className="navLine" ref={mobileNav2}/>
+            <div className="navLine" ref={mobileNav3}/>
+          </div>
+        }
+        <ul ref={navBarRef}>
+          {window.innerWidth < 1000 &&
+            <p className="redPrintPick">Print Pick</p>
+          }
           <li>
             <Link className="navLink" to="/">
               Home
